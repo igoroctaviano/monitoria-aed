ec72aad30cb991d5115515db261290d377740b6c
Igor Octaviano Ribeiro Rezende <igorocta@outlook.com>
2016-01-12 14:42:53 -0200

    Initial Commit.

---
+++ 501119-MetdLista/501119-MetdLista/Node.cs
@@ -0,0 +1,36 @@
+﻿//
+// Pontifícia Universidade Católica de Minas Gerais
+// Unidade São Gabriel
+// Disciplina: Algoritmos e Estrutura de Dados
+// *Algorithms and Data Structures
+// 
+// Igor Octaviano
+// More? access: https://github.com/igoroctaviano/
+// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _501119_MetdLista
+{
+    class Node
+    {
+        public Object item;
+        public Node next;
+        public Node prev;
+
+        public Node(Object item, Node next, Node prev)
+        {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public Node()
+        {
+            item = next = prev = null;
+        }
+    }
+}
